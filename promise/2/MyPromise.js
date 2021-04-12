692c52469430176cba6de0fe796a03447a541662
xu.xiao <xu.xiao@zhaopin.com.cn>
2021-04-12 20:51:33 +0800

    =1=定义执行器函数，及其resolve,reject函数参数；初始化三个变量

---
+++ promise/2/MyPromise.js
@@ -0,0 +1,24 @@
+// 定义三个状态常量
+const PENDING = 'pending',
+      FULFILLED = 'fulfilled',
+      REJECTED = 'rejected';
+
+class MyPromise {
+  // 存储状态，初始为pending
+  status = PENDING
+  // 成功后的值
+  value = ''
+  // 失败后的值
+  reason = ''
+
+  constructor (executer) {
+    // executer会立即执行，传入resolve,reject两个函数参数
+    executer(this.resolve, this.reject)
+  }
+
+  // 使用箭头函数保证this指向当前实例对象
+  // 执行器中传入的resolve函数，用来将状态变为fulfilled
+  resolve = () => {}
+  // 执行器中传入的reject函数，用来将状态变为rejected
+  reject = () => {}
+}
