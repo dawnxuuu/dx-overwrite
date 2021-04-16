ceca968e8679d5e340498bc97b4faef89991dc0f
xu.xiao <xu.xiao@zhaopin.com.cn>
2021-04-16 10:24:43 +0800

    =2=resolve和reject函数简单实现

--- promise/2/MyPromise.js
+++ promise/2/MyPromise.js
@@ -18,7 +18,24 @@ class MyPromise {
 
   // 使用箭头函数保证this指向当前实例对象
   // 执行器中传入的resolve函数，用来将状态变为fulfilled
-  resolve = () => {}
+  resolve = (value) => {
+    // 只有状态是等待，才执行状态修改
+    if (this.status === PENDING) {
+      // 状态改为成功
+      this.status = FULFILLED
+      // 保存成功后的值
+      this.value = value
+    }
+  }
+
   // 执行器中传入的reject函数，用来将状态变为rejected
-  reject = () => {}
+  reject = () => {
+    // 只有状态是等待，才执行状态修改
+    if (this.status === PENDING) {
+      // 状态改为失败
+      this.status = REJECTED
+      // 保存失败后的值
+      this.reason = reason
+    }
+  }
 }
