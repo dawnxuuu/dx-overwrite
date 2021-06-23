237e03f2b448e073204b88edf9d4751d2d723154
xu.xiao <xu.xiao@zhaopin.com.cn>
2021-06-23 21:49:17 +0800

    =17=添加单元测试辅助工具依赖

--- promise/2/MyPromise.js
+++ promise/2/MyPromise.js
@@ -147,3 +147,16 @@ function handleResult (anotherPromise, result, resolve, reject) {
     resolve(result)
   }
 }
+
+
+MyPromise.deferred = function () {
+  var result = {}
+  result.promise = new MyPromise(function (resolve, reject) {
+    result.resolve = resolve
+    result.reject = reject
+  })
+
+  return result
+}
+
+module.exports = MyPromise
